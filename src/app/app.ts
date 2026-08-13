ad857f2571d8532d56f811eccb624c6d1fb94510
Matthew Bischoff <matthew.john.bischoff@gmail.com>
2026-08-13 13:29:58 -0400

    first pass

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { Jumbotron } from './jumbotron/jumbotron';
+import { Services } from './services/services';
+import { HowItWorks } from './how-it-works/how-it-works';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Jumbotron, Services, HowItWorks, Footer],
   templateUrl: './app.html',
   styleUrl: './app.less'
 })
---
+++ src/app/jumbotron/jumbotron.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jumbotron',
+  imports: [],
+  templateUrl: './jumbotron.html',
+  styleUrl: './jumbotron.less',
+})
+export class Jumbotron {
+
+}
